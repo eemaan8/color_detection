f9859fc186168412a7e75d2e345fb4ded9a6e7e1
eemaan8 <107769219+eemaan8@users.noreply.github.com>
2025-05-25 17:54:33 +0500

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
-from color_logic import get_color_name  # Import shared logic
+from color_logic import get_color_name  # Make sure this file is deployed
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Color Detection API is running!"
 
 @app.route('/detect_color', methods=['GET', 'POST'])
 def detect_color():
@@ -10,7 +14,7 @@ def detect_color():
         R = data.get('R')
         G = data.get('G')
         B = data.get('B')
-    else:  # GET method
+    else:
         R = request.args.get('R', type=int)
         G = request.args.get('G', type=int)
         B = request.args.get('B', type=int)
@@ -22,4 +26,6 @@ def detect_color():
     return jsonify({'color': color_name})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
