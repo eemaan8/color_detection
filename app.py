7286979820042d44b777e2c8914de6f57f1d80a9
eemaan8 <107769219+eemaan8@users.noreply.github.com>
2025-06-06 19:30:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
-from color_logic import get_color_name  # Make sure this file is deployed
+from color_logic import get_color_name
+import cv2
+import numpy as np
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
@@ -7,23 +11,31 @@ app = Flask(__name__)
 def home():
     return "Color Detection API is running!"
 
-@app.route('/detect_color', methods=['GET', 'POST'])
+@app.route('/detect_color', methods=['POST'])
 def detect_color():
-    if request.method == 'POST':
-        data = request.get_json()
-        R = data.get('R')
-        G = data.get('G')
-        B = data.get('B')
-    else:
-        R = request.args.get('R', type=int)
-        G = request.args.get('G', type=int)
-        B = request.args.get('B', type=int)
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
 
-    if R is None or G is None or B is None:
-        return jsonify({'error': 'Missing RGB values'}), 400
+    image_file = request.files['image']
 
+    # Read image as numpy array
+    image = Image.open(image_file.stream).convert('RGB')
+    image_np = np.array(image)
+
+    # Get center pixel
+    height, width, _ = image_np.shape
+    center_y, center_x = height // 2, width // 2
+    R, G, B = image_np[center_y, center_x]
+
+    # Get color name from CSV
     color_name = get_color_name(R, G, B)
-    return jsonify({'color': color_name})
+
+    return jsonify({
+        'R': int(R),
+        'G': int(G),
+        'B': int(B),
+        'color': color_name
+    })
 
 if __name__ == '__main__':
     import os
