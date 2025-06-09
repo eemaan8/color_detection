ccf6ea06337ce1c70e031588e0f7d270a70effeb
eemaan8 <107769219+eemaan8@users.noreply.github.com>
2025-06-09 05:36:29 +0500

    Update color_logic.py

--- color_logic.py
+++ color_logic.py
@@ -1,18 +1,21 @@
-# color_logic.py
 import pandas as pd
 
-# Load color data (CSV should be in same folder)
 df = pd.read_csv('color.csv')
 
 def get_color_name(R, G, B):
     min_diff = float('inf')
     color_name = ""
+
     for i in range(len(df)):
         csv_r = int(df.loc[i, 'R'])
         csv_g = int(df.loc[i, 'G'])
         csv_b = int(df.loc[i, 'B'])
-        diff = abs(R - csv_r) + abs(G - csv_g) + abs(B - csv_b)
+
+        # Calculate Euclidean distance
+        diff = ((R - csv_r) ** 2 + (G - csv_g) ** 2 + (B - csv_b) ** 2) ** 0.5
+
         if diff < min_diff:
             min_diff = diff
             color_name = df.loc[i, 'ColorName']
+
     return color_name
